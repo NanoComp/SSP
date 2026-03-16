e4e53df381037972359eb53fe17b3c956296edb0
Lorenzo Van Munoz <lorenzo@vanmunoz.com>
2026-03-16 15:20:46 -0400

    refactor projection into a loop over target points

--- src/julia/SSP/src/project.jl
+++ src/julia/SSP/src/project.jl
@@ -24,12 +24,17 @@ end
 
 function init(prob::ProjectionProblem, alg::SSP2)
     (; data, grid, target_points) = prob
-    interp_output = similar(data, length(target_points))
-    interp_gradient_output = similar(data, length(target_points), length(grid))
-    interp_hessian_output = similar(data, length(target_points), length(grid), length(grid))
+    # TODO refactor these workspaces as views of a single array
+    rho_filtered = similar(data, length(target_points))
+    rho_filtered_gradient = similar(data, length(target_points), length(grid))
+    rho_filtered_hessian = similar(data, length(target_points), length(grid), length(grid))
+    rho_projected = similar(data, length(target_points))
     adj_data = similar(data)
     adj_data_tmp = similar(data)
-    cacheval = (; interp_output, interp_gradient_output, interp_hessian_output, adj_data, adj_data_tmp)
+    adj_rho_filtered = similar(rho_filtered)
+    adj_rho_filtered_gradient = similar(rho_filtered_gradient)
+    adj_rho_filtered_hessian = similar(rho_filtered_hessian)
+    cacheval = (; rho_filtered, rho_filtered_gradient, rho_filtered_hessian, rho_projected, adj_data, adj_data_tmp, adj_rho_filtered, adj_rho_filtered_gradient, adj_rho_filtered_hessian)
     return ProjectionSolver(data, grid, target_points, alg, cacheval)
 end
 
@@ -40,11 +45,22 @@ end
 function proj_solve!(solver, alg::SSP2)
 
     (; data, grid, target_points, cacheval) = solver
-    (; interp_output, interp_gradient_output, interp_hessian_output) = cacheval
+    (; rho_filtered, rho_filtered_gradient, rho_filtered_hessian, rho_projected) = cacheval
 
-    interpolatevaluegradienthessian!!!(interp_output, interp_gradient_output, interp_hessian_output, grid, data, target_points)
-    value, tape = smoothed_projection(interp_output, interp_gradient_output, interp_hessian_output, grid, alg.beta, alg.eta)
-    return (; value, tape)
+    interpolatevaluegradienthessian!!!(rho_filtered, rho_filtered_gradient, rho_filtered_hessian, grid, data, target_points)
+    dx_all = step.(grid)
+    @assert allequal(dx_all)
+    dx = first(dx_all)
+    R_smoothing = 0.55 * dx
+    for (i, rho_f, rho_g, rho_h) in zip(eachindex(rho_projected), rho_filtered, eachslice(rho_filtered_gradient; dims=1), eachslice(rho_filtered_hessian; dims=1))
+        # the calculation of the norm is not local in memory, but this is
+        # because the interpolation is done as SoA whereas here we use AoS
+        rho_g_normsq = sum(abs2, rho_g)
+        rho_h_normsq = sum(abs2, rho_h)
+        rho_p, tape = smoothed_projection(rho_f, rho_g_normsq, rho_h_normsq, R_smoothing, alg.beta, alg.eta)
+        rho_projected[i] = rho_p
+    end
+    return (; value=rho_projected, tape=nothing)
 end
 
 function interpolatevaluegradienthessian!!!(interp_output, interp_gradient_output, interp_hessian_output, grid, data, target_points)
@@ -95,35 +111,30 @@ function adjoint_tanh_projection(adj_out, x, beta, eta)
 end
 
 
-function smoothed_projection(rho_filtered_vals, rho_filtered_gradient_vals, rho_filtered_hessian_vals, grids, beta, eta)
-    dx_all = step.(grids)
-    @assert allequal(dx_all)
-    dx = first(dx_all)
-    R_smoothing = 0.55 * dx
+function smoothed_projection(rho_filtered, rho_filtered_gradient_normsq, rho_filtered_hessian_normsq, R_smoothing, beta, eta)
+    rho_projected = tanh_projection(rho_filtered, beta, eta)
 
-    rho_projected = tanh_projection.(rho_filtered_vals, beta, eta)
+    den_helper = rho_filtered_gradient_normsq + R_smoothing^2 * rho_filtered_hessian_normsq
 
-    den_helper = vec(mapreduce(abs2, +, rho_filtered_gradient_vals; dims=2))
-    den_helper .+= R_smoothing^2 .* vec(mapreduce(abs2, +, rho_filtered_hessian_vals; dims=(2, 3)))
+    nonzero_norm = abs(den_helper) > 0
 
-    nonzero_norm = abs.(den_helper) .> 0
+    den_eff = sqrt(ifelse(nonzero_norm, den_helper, oneunit(den_helper)))
+    d = (eta - rho_filtered) / den_eff
+    d_R = d / R_smoothing
 
-    den_norm = sqrt.(ifelse.(nonzero_norm, den_helper, one(eltype(den_helper))))
-    den_eff = ifelse.(nonzero_norm, den_norm, one(eltype(den_norm)))
-    d = (eta .- rho_filtered_vals) ./ den_eff
-    needs_smoothing = nonzero_norm .& (abs.(d) .< R_smoothing)
-    d_R = d ./ R_smoothing
-    F_plus = ifelse.(needs_smoothing, 1//2 .+ d_R .* evalpoly.(d_R.^2, Ref((-15//16, 5//8, -3//16))), 1.0)
-    F_minus = ifelse.(needs_smoothing, 1//2 .+ d_R .* evalpoly.(d_R.^2, Ref((15//16, -5//8, 3//16))), 1.0)
-    rho_filtered_minus = rho_filtered_vals .- R_smoothing .* den_eff .* F_plus
-    rho_filtered_plus = rho_filtered_vals .+ R_smoothing .* den_eff .* F_minus
+    needs_smoothing = nonzero_norm & (abs(d_R) < one(d_R))
+    F_plus  = ifelse(needs_smoothing, 1//2 + d_R * evalpoly(d_R^2, (-15//16, 5//8, -3//16)), one(d_R))
+    F_minus = ifelse(needs_smoothing, 1//2 + d_R * evalpoly(d_R^2, (15//16, -5//8, 3//16)),  one(d_R))
 
-    rho_minus_eff_projected = tanh_projection.(rho_filtered_minus, beta, eta)
-    rho_plus_eff_projected = tanh_projection.(rho_filtered_plus, beta, eta)
+    rho_filtered_minus = rho_filtered - R_smoothing * den_eff * F_plus
+    rho_filtered_plus  = rho_filtered + R_smoothing * den_eff * F_minus
 
-    rho_projected_smoothed = (1 .- F_plus) .* rho_minus_eff_projected .+ F_plus .* rho_plus_eff_projected
-    tape = (; F_plus, F_minus, d, d_R, rho_projected, den_helper, den_eff, den_norm, nonzero_norm, needs_smoothing, rho_filtered_minus, rho_filtered_plus, rho_minus_eff_projected, rho_plus_eff_projected)
-    return ifelse.(needs_smoothing, rho_projected_smoothed, rho_projected), tape
+    rho_minus_eff_projected = tanh_projection(rho_filtered_minus, beta, eta)
+    rho_plus_eff_projected  = tanh_projection(rho_filtered_plus,  beta, eta)
+
+    rho_projected_smoothed = (1 - F_plus) * rho_minus_eff_projected + F_plus * rho_plus_eff_projected
+    tape = (; F_plus, F_minus, d, d_R, rho_projected, den_helper, den_eff, nonzero_norm, needs_smoothing, rho_filtered_minus, rho_filtered_plus, rho_minus_eff_projected, rho_plus_eff_projected)
+    return ifelse(needs_smoothing, rho_projected_smoothed, rho_projected), tape
 end
 
 
@@ -134,13 +145,27 @@ end
 function adjoint_proj_solve!(solver, alg::SSP2, adj_sol, tape)
 
     (; data, grid, target_points, cacheval) = solver
-    (; interp_output, interp_gradient_output, interp_hessian_output, adj_data, adj_data_tmp) = cacheval
+    (; rho_filtered, rho_filtered_gradient, rho_filtered_hessian, adj_data, adj_data_tmp, adj_rho_filtered, adj_rho_filtered_gradient, adj_rho_filtered_hessian) = cacheval
 
-    # need to recompute interpolation since the data may have changed since the forward pass
-    interpolatevaluegradienthessian!!!(interp_output, interp_gradient_output, interp_hessian_output, grid, data, target_points)
+    # We do not keep a tape and need to repeat the forward calculation
+    interpolatevaluegradienthessian!!!(rho_filtered, rho_filtered_gradient, rho_filtered_hessian, grid, data, target_points)
 
-    adj_interp_output, adj_interp_gradient_output, adj_interp_hessian_output = adjoint_smoothed_projection(adj_sol, tape, interp_output, interp_gradient_output, interp_hessian_output, grid, alg.beta, alg.eta)
-    adjoint_interpolatevaluegradienthessian!!(adj_data, adj_data_tmp, adj_interp_output, adj_interp_gradient_output, adj_interp_hessian_output, grid, target_points)
+    dx_all = step.(grid)
+    @assert allequal(dx_all)
+    dx = first(dx_all)
+    R_smoothing = 0.55 * dx
+    for (i, adj_proj, rho_f, rho_g, rho_h) in zip(eachindex(adj_rho_filtered), adj_sol, rho_filtered, eachslice(rho_filtered_gradient; dims=1), eachslice(rho_filtered_hessian; dims=1))
+        # the calculation of the norm is not local in memory, but this is
+        # because the interpolation is done as SoA whereas here we use AoS
+        rho_g_normsq = sum(abs2, rho_g)
+        rho_h_normsq = sum(abs2, rho_h)
+        rho_p, tape = smoothed_projection(rho_f, rho_g_normsq, rho_h_normsq, R_smoothing, alg.beta, alg.eta)
+        adj_rho_f, adj_rho_g_normsq, adj_rho_h_normsq = adjoint_smoothed_projection(adj_proj, tape, rho_f, rho_g_normsq, rho_h_normsq, R_smoothing, alg.beta, alg.eta)
+        adj_rho_filtered[i] = adj_rho_f
+        adj_rho_filtered_gradient[i, :] .= 2adj_rho_g_normsq .* rho_g
+        adj_rho_filtered_hessian[i, :, :] .= 2adj_rho_h_normsq .* rho_h
+    end
+    adjoint_interpolatevaluegradienthessian!!(adj_data, adj_data_tmp, adj_rho_filtered, adj_rho_filtered_gradient, adj_rho_filtered_hessian, grid, target_points)
 
     return (; data=adj_data, grid=nothing, target_points=nothing)
 end
@@ -170,34 +195,34 @@ function adjoint_interpolatevaluegradienthessian!!(adj_data, adj_data_tmp, adj_i
     return
 end
 
-function adjoint_smoothed_projection(adj_rho_ssp_projected, tape, rho_filtered_vals, rho_filtered_gradient_vals, rho_filtered_hessian_vals, grids, beta, eta)
-    (; F_plus, F_minus, d, d_R, rho_projected, den_helper, den_eff, den_norm, nonzero_norm, needs_smoothing, rho_filtered_minus, rho_filtered_plus, rho_minus_eff_projected, rho_plus_eff_projected) = tape
-    dx_all = step.(grids)
-    @assert allequal(dx_all)
-    dx = first(dx_all)
-    R_smoothing = 0.55 * dx
 
-    adj_rho_projected = adj_rho_ssp_projected .* .!needs_smoothing
-    adj_rho_filtered_vals = adjoint_tanh_projection.(adj_rho_projected, rho_filtered_vals, beta, eta)
+function adjoint_smoothed_projection(adj_rho_projected_maybe_smoothed, tape, rho_filtered, rho_filtered_gradient_normsq, rho_filtered_hessian_normsq, R_smoothing, beta, eta)
+    (; F_plus, F_minus, d, d_R, rho_projected, den_helper, den_eff, nonzero_norm, needs_smoothing, rho_filtered_minus, rho_filtered_plus, rho_minus_eff_projected, rho_plus_eff_projected) = tape
 
-    adj_rho_projected_smoothed = adj_rho_ssp_projected .* needs_smoothing
-    adj_rho_plus_eff_projected = adj_rho_projected_smoothed .* F_plus
-    adj_rho_minus_eff_projected = adj_rho_projected_smoothed .* (1 .- F_plus)
-    adj_rho_filtered_minus = adjoint_tanh_projection.(adj_rho_minus_eff_projected, rho_filtered_minus, beta, eta)
-    adj_rho_filtered_plus = adjoint_tanh_projection.(adj_rho_plus_eff_projected, rho_filtered_plus, beta, eta)
-    adj_rho_filtered_vals .+= adj_rho_filtered_minus .+ adj_rho_filtered_plus
-    adj_F_plus = .-adj_rho_filtered_minus .* R_smoothing .* den_eff .+ adj_rho_projected_smoothed .* (rho_plus_eff_projected .- rho_minus_eff_projected)
-    adj_den_eff = R_smoothing .* (adj_rho_filtered_plus .* F_minus .- adj_rho_filtered_minus .* F_plus)
-    adj_F_minus = adj_rho_filtered_plus .* R_smoothing .* den_eff
-    adj_d_R = ifelse.(needs_smoothing, adj_F_plus .* evalpoly.(d_R.^2, Ref((-15//16, 15//8, -15//16))) .+ adj_F_minus .* evalpoly.(d_R.^2, Ref((15//16, -15//8, 15//16))), 0.0)
-    adj_d = adj_d_R ./ R_smoothing
-    adj_rho_filtered_vals .-= adj_d ./ den_eff
-    adj_den_eff .-= adj_d .* d ./ den_eff
-    adj_den_norm = ifelse.(nonzero_norm, adj_den_eff, 0.0)
-    adj_den_helper = ifelse.(nonzero_norm, adj_den_norm ./ (2 .* den_norm), 0.0)
-    adj_rho_filtered_gradient_vals = 2 .* adj_den_helper .* rho_filtered_gradient_vals
-    adj_rho_filtered_hessian_vals = R_smoothing^2 .* 2 .* adj_den_helper .* rho_filtered_hessian_vals
+    adj_rho_projected = ifelse(needs_smoothing, zero(adj_rho_projected_maybe_smoothed), adj_rho_projected_maybe_smoothed)
+    adj_rho_filtered  = adjoint_tanh_projection(adj_rho_projected, rho_filtered, beta, eta)
 
-    return adj_rho_filtered_vals, adj_rho_filtered_gradient_vals, adj_rho_filtered_hessian_vals
+    adj_rho_projected_smoothed  = ifelse(needs_smoothing, adj_rho_projected_maybe_smoothed, zero(adj_rho_projected_maybe_smoothed))
+    adj_rho_plus_eff_projected  = adj_rho_projected_smoothed * F_plus
+    adj_rho_minus_eff_projected = adj_rho_projected_smoothed * (1 - F_plus)
+
+    adj_rho_filtered_minus = adjoint_tanh_projection(adj_rho_minus_eff_projected, rho_filtered_minus, beta, eta)
+    adj_rho_filtered_plus  = adjoint_tanh_projection(adj_rho_plus_eff_projected,  rho_filtered_plus, beta, eta)
+    adj_rho_filtered += adj_rho_filtered_minus + adj_rho_filtered_plus
+
+    adj_F_plus = -adj_rho_filtered_minus * R_smoothing * den_eff + adj_rho_projected_smoothed * (rho_plus_eff_projected - rho_minus_eff_projected)
+    adj_den_eff = R_smoothing * (adj_rho_filtered_plus * F_minus - adj_rho_filtered_minus * F_plus)
+    adj_F_minus = adj_rho_filtered_plus * R_smoothing * den_eff
+    adj_d_R = ifelse(needs_smoothing, adj_F_plus * evalpoly(d_R^2, (-15//16, 15//8, -15//16)) + adj_F_minus * evalpoly(d_R^2, (15//16, -15//8, 15//16)), zero(adj_F_plus) + zero(adj_F_minus))
+    
+    adj_d = adj_d_R / R_smoothing
+    adj_rho_filtered -= adj_d / den_eff
+    adj_den_eff -= adj_d * d / den_eff
+    adj_den_helper = ifelse(nonzero_norm, adj_den_eff, zero(adj_den_eff)) / 2den_eff
+
+    adj_rho_filtered_gradient_normsq = adj_den_helper
+    adj_rho_filtered_hessian_normsq = adj_den_helper * R_smoothing^2
+
+    return adj_rho_filtered, adj_rho_filtered_gradient_normsq, adj_rho_filtered_hessian_normsq
 end
 end
